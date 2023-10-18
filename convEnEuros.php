a8c7842ff4deb82f268235e8c54d8493c3ef9363
Laetitiasd <144696523+Laetitiasd@users.noreply.github.com>
2023-10-18 17:20:26 +0200

    Add files via upload

---
+++ convEnEuros.php
@@ -0,0 +1,27 @@
+<?php
+echo "Saisir la devise : ";
+$devise = strtoupper(rtrim(fgets(STDIN)));
+
+if ($devise == "USD") {
+    $tauxChange = 0.883;
+} elseif ($devise == "CAD") {
+    $tauxChange = 0.686;
+} elseif ($devise == "GBP") {
+    $tauxChange = 1.154;
+} elseif ($devise == "RUB") {
+    $tauxChange = 0.013;
+} else {
+    $tauxChange = 0;
+}
+
+if ($tauxChange == 0) {
+    echo "Devise inconnue. \n";
+} else {
+    echo "Saisir le prix en ", $devise, " : ";
+    $prix = rtrim(fgets(STDIN));
+
+    if (is_numeric($prix)) {
+        $prixEuros = $prix * $tauxChange;
+        echo "Prix en euros : ", $prixEuros, " euros.\n";
+    }
+}
