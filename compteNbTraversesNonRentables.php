0f56aa688836ae6e90ed906e0d84c15fdb5fd7cd
Laetitiasd <144696523+Laetitiasd@users.noreply.github.com>
2023-12-06 17:11:02 +0100

    Add files via upload

--- compteNbTraversesNonRentables.php
+++ compteNbTraversesNonRentables.php
@@ -2,4 +2,12 @@
 	
 	$nbPassagers = array(35,10,22,52,15,58,8,73);
 	
+	$nbTotal = 0;
+	
 for($i = 0; $i< count($nbPassagers); $i++) {
+	if($nbPassagers[$i] < 20){
+		$nbTotal = $nbTotal + 1;
+}
+}
+	echo $nbTotal . " traversée non rentable le 22/09/2022. \n";
+?>
