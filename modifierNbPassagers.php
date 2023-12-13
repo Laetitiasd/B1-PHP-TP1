7804782ee52746b3be82077a9b2a8d0fc1759c6e
Laetitiasd <144696523+Laetitiasd@users.noreply.github.com>
2023-12-13 16:38:54 +0100

    modifierNbPassagers.php

--- modifierNbPassagers.php
+++ modifierNbPassagers.php
@@ -1,1 +1,12 @@
+<?php
+
+$nbPassagers = array(35, 10, 22, 52, 15, 58, 8, 73);
+
+foreach ($nbPassagers as $passager) {
+    $passagerArrondi = ceil($passager / 10) * 10;
+    
+    echo $passagerArrondi . " passagers\n";
+}
+
+?>
 
